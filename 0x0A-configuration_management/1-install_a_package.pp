bd94a8872cfead8e6184b728a9523e5cabbf9933
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-02-23 14:33:48 +0200

    .

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Install Flask 2.1.0 using pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
