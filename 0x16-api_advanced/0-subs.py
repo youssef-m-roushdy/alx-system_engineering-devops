d58efb7d7004106c9b2d7742cbba5188ea97a8d0
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-04-10 15:47:19 +0200

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    sub_info = requests.get(url,
-                            headers={"User-Agent": "My-User-Agent yousef"},
-                            allow_redirects=False)
-    if sub_info.status_code == 404:
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
