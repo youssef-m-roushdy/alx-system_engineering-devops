f9bb68494d503e0208c4254cbfbcc6b42d3d22b0
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-04-10 16:11:13 +0200

    .

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+        function that queries the Reddit API and prints
+        the titles of the first 10 hot posts
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'Custom User Agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        if posts:
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print("No posts found.")
+    else:
+        print("None")
