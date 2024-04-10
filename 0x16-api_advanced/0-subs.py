bf49ef06f85898a9bb27c69dfbad1c6f07976753
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-04-10 15:09:36 +0200

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """Module for task 0"""
+import requests
 
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    sub_info = requests.get(url,
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-    if sub_info.status_code >= 300:
+    if sub_info.status_code == 404:
         return 0
 
     return sub_info.json().get("data").get("subscribers")
