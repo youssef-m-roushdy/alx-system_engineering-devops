a02da69e53dd49960d4be1a01221dabb400c2b95
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-06-04 23:34:02 +0300

    .

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of total subscribers for a given
-subreddit.
+0-main
 """
-import requests
+import sys
 
-
-def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of total subscribers for a
-    given subreddit.
-    """
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Redit-API'}
-    response = requests.get(url, headers=headers)
-    if (not response.ok):
-        return 0
-    subscriber_count = response.json().get('data').get('subscribers')
-    return subscriber_count
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
