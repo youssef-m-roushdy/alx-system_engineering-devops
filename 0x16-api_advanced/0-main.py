9bf04f82355eaf2cdd80a86df63914d93f2fbd8d
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-06-04 23:32:50 +0300

    .

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     given subreddit.
     """
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    headers = {'User-Agent': 'Redit-API'}
     response = requests.get(url, headers=headers)
     if (not response.ok):
         return 0
